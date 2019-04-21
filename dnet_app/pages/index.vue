afd5bba13d060de9f75226b339c5c0c23032f64d
Gagan Daroach <gagandaroach@gmail.com>
2019-04-21 01:57:52 -0500

    simplify main screen stylings.

--- dnet_app/pages/index.vue
+++ dnet_app/pages/index.vue
@@ -8,7 +8,7 @@
         </b-alert>
       </div>
       <logo />
-      <h1 class="title">
+      <h1 class="display-2">
         daroach.net
       </h1>
       <Nav />
@@ -36,27 +36,5 @@ export default {
   justify-content: center;
   align-items: center;
   text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
 }
 </style>
