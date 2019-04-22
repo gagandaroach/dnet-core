f6c6d77b8950b98434de37af06ce17018715be91
Gagan Daroach <gagandaroach@gmail.com>
2019-04-21 19:34:23 -0500

    add container margins
    add sticky prop. secondary silver styling

--- dnet_app/components/Navbar.vue
+++ dnet_app/components/Navbar.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-    <b-navbar toggleable="md" type="dark" variant="dark">
-      <b-navbar-brand to="/">daroach.net</b-navbar-brand>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav class="ml-auto">
-          <b-nav-item to="/about" right>about</b-nav-item>
-          <b-nav-item to="/blog" right>blog</b-nav-item>
-          <b-nav-item href="https://cloud.daroach.net/" right>cloud</b-nav-item>
-          <b-nav-item-dropdown id="nav-admin-dropdown" text="admin" right>
-            <b-dropdown-item href="https://portainer.daroach.net"
-              >portainer</b-dropdown-item
+    <b-navbar type="dark" variant="secondary" sticky>
+      <b-container>
+        <b-navbar-brand to="/">daroach.net</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item to="/about" active-class="active" right
+              >About</b-nav-item
             >
-          </b-nav-item-dropdown>
-        </b-navbar-nav>
-      </b-collapse>
+            <b-nav-item to="/blog" active-class="active" right>Blog</b-nav-item>
+            <b-nav-item href="https://cloud.daroach.net/" right
+              >Cloud</b-nav-item
+            >
+            <b-nav-item-dropdown id="nav-admin-dropdown" text="Misc" right>
+              <b-dropdown-item href="https://portainer.daroach.net"
+                >Portainer</b-dropdown-item
+              >
+            </b-nav-item-dropdown>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-container>
     </b-navbar>
   </div>
 </template>
