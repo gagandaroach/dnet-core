48a2eb73f166572a0d281e5d9a3daa15fe716f5e
Gagan Daroach <gagandaroach@gmail.com>
2019-05-26 17:45:39 -0500

    about update - copy over template from old site, compiling nuxtjs project

--- dnet_app/pages/about.vue
+++ dnet_app/pages/about.vue
@@ -1,18 +1,206 @@
 <template>
   <section>
     <Navbar />
-    <PageTitle title="About" description="A Work In Progress" />
+    <br />
+    <!--  -->
+    <div class="container-fluid text-center">
+      <div class="jumbotron">
+        <h1>Gagandeep Bishamber Daroach</h1>
+        <br />
+        <div class="row">
+          <div class="container col-md-6 col-xl-12">
+            <p class="lead">Daroach.net Maintainer and Host</p>
+            <hr class="my-2" />
+            <p>
+              I am an aspiring Software Engineer, hoping to
+              <a href="https://youtu.be/J-GVd_HLlps?t=32"
+                >make the world a better place</a
+              >. With this website, I hope to create a place for people to share
+              stories, knowledge, information, and data.
+            </p>
+            <div class="social-media-icons col-xs-12">
+              <ul class="list-inline col-xs-12">
+                <a href="https://www.facebook.com/gagany6">
+                  <i class="fa fa-facebook-square fa-4x"></i>
+                </a>
+                <a href="https://github.com/gagandaroach">
+                  <i class="fa fa-github-square fa-4x"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/gdaroach/">
+                  <i class="fa fa-linkedin-square fa-4x"></i>
+                </a>
+                <a href="https://twitter.com/gagany1">
+                  <i class="fa fa-twitter-square fa-4x"></i>
+                </a>
+                <a href="https://www.instagram.com/galapagosislands/">
+                  <i class="fa fa-instagram fa-4x"></i>
+                </a>
+              </ul>
+            </div>
+          </div>
+          <div class="container col-md-6">
+            <img src="~/assets/outside_home_tree.jpeg" class="img-fluid" alt />
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid text-center">
+      <div class="jumbotron">
+        <h1 class="display-7">I am the result of two wonderful parents.</h1>
+
+        <div class="container">
+          <br />
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-header">
+                  <h5>Gurdeep Daroach</h5>
+                </div>
+                <a href="https://www.facebook.com/gurdeep.daroach">
+                  <img
+                    class="card-img-top"
+                    src="/static/images/mom.jpg"
+                    alt="My Mother, Gurdeep Daroach"
+                  />
+                </a>
+                <figcaption class="figure-caption text-right">
+                  Photo by Gagan Daroach&nbsp;
+                </figcaption>
+                <div class="card-body">
+                  <p class="card-text">The most loving mother in the world.</p>
+                </div>
+              </div>
+            </div>
+            <br />
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-header">
+                  <h5>Gian Daroach</h5>
+                </div>
+                <a
+                  href="https://www.aurorahealthcare.org/doctors/dr-gian-c-daroach-md"
+                >
+                  <img
+                    class="card-img-top"
+                    src="/static/images/dad.jpg"
+                    alt="Picture of Site Maintainer and Host, Gagan Daroach"
+                  />
+                </a>
+                <figcaption class="figure-caption text-right">
+                  Photo by Gagan Daroach&nbsp;
+                </figcaption>
+                <div class="card-body">
+                  <p class="card-text">
+                    A kind man who set the bar at the highest possible position.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container-fluid text-center">
+      <div class="jumbotron">
+        <div class="container">
+          <h1>Where do I spend my time?</h1>
+          <br />
+          <div class="row">
+            <div class>
+              <div class="card">
+                <div class="card-header">
+                  <h5>Milwaukee, Wisconsin</h5>
+                </div>
+                <a href="https://en.wikipedia.org/wiki/Milwaukee">
+                  <img
+                    class="card-img-top"
+                    src="~/assets/milwaukee.JPG"
+                    alt="Milwaukee Wisconsin"
+                  />
+                </a>
+                <figcaption class="figure-caption text-right">
+                  Photo by Gagan Daroach&nbsp;
+                </figcaption>
+                <div class="card-body">
+                  <p class="card-text">
+                    I currently live in Milwaukee, Wisconsin. I love this city,
+                    and I love this state.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <br />
+          </div>
+          <br />
+          <div class="row">
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-header">
+                  <h5>Milwaukee School of Engineering</h5>
+                </div>
+                <a href="https://msoe.edu">
+                  <img
+                    class="card-img-top"
+                    src="https://s3.amazonaws.com/msoe/files/resources/msoe-student-life-and-campus-center-exterior-2.jpg"
+                    alt="Milwaukee School of Engineering Campus Center"
+                  />
+                </a>
+                <figcaption class="figure-caption text-right">
+                  Image from msoe.edu&nbsp;
+                </figcaption>
+                <div class="card-body">
+                  <p class="card-text">
+                    My current school, here I am pursuing an undergraduate
+                    degree in Software Engineering. The Milwaukee School of
+                    Engineering is, in my own words, an institution whose best
+                    characteristic is an enviroment that allows for students to
+                    focus on their engineering education.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <br />
+            <div class="col-sm-6">
+              <div class="card">
+                <div class="card-header">
+                  <h5>Cognex Corporation</h5>
+                </div>
+                <a href="https://cognex.com">
+                  <img
+                    class="card-img-top"
+                    src="https://www.cognex.com/library/media/products/in-sight-2000-vision-sensors/features/is2000_explode2.jpg?h=562&w=1000&la=en&hash=12A9D8F4DCF56F62352D98DAD8092A663819B28F"
+                    alt="Cognex Merlin Camera Exploded"
+                  />
+                </a>
+                <figcaption class="figure-caption text-right">
+                  Image from cognex.com&nbsp;
+                </figcaption>
+                <div class="card-body">
+                  <p class="card-text">
+                    My current workplace, Cognex is a camera company full of
+                    exceptionally nice, hard working nerds. Here I work as a
+                    Software Engineering Intern.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- End Gagan Daroach Content -->
   </section>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
-import PageTitle from '~/components/PageTitle.vue'
 
 export default {
   components: {
-    Navbar,
-    PageTitle
+    Navbar
   }
 }
 </script>
