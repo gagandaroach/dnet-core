6fce1726b2429c965ff22749025ceaee89571d16
Gagan Daroach <gagandaroach@gmail.com>
2019-05-26 23:14:31 -0500

    add bootstrap image config change to vue loader

--- dnet_app/nuxt.config.js
+++ dnet_app/nuxt.config.js
@@ -57,6 +57,22 @@ export default {
           exclude: /(node_modules)/
         })
       }
+      const vueLoader = config.module.rules.find(
+        rule => rule.loader === 'vue-loader'
+      )
+      vueLoader.options.transformAssetUrls = {
+        video: ['src', 'poster'],
+        source: 'src',
+        img: 'src',
+        image: 'xlink:href',
+        'b-img': 'src',
+        'b-img-lazy': ['src', 'blank-src'],
+        'b-card': 'img-src',
+        'b-card-img': 'img-src',
+        'b-card-img-lazy': ['src', 'blank-src'],
+        'b-carousel-slide': 'img-src',
+        'b-embed': 'src'
+      }
     }
   }
 }
