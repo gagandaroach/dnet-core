1f240187dcde22fa40a65b8133140b995638180f
Gagan Daroach <gagandaroach@gmail.com>
2019-05-29 12:09:08 -0500

    add server rules, how to connect image, and server features

--- dnet_app/pages/minecraft.vue
+++ dnet_app/pages/minecraft.vue
@@ -3,25 +3,87 @@
     <Navbar />
 
     <PageTitle
-      title="Minecraft Server"
+      title="Daroach.net Minecraft"
       description="A fun place to procrastinate your life's responsibiliteis."
     />
+
+    <hr />
 
     <b-container>
       <b-row id="rules">
         <b-col>
-          <h1>Server Rules</h1>
-          <ol>
-            <li>Treat thy neighbor how one would treat themself.</li>
-            <li></li>
-          </ol>
+          <b-card
+            no-body="true"
+            border-variant="danger"
+            body-border-variant="danger"
+          >
+            <b-card-header
+              header="Server Rules"
+              class="display-4"
+              header-text-variant="danger"
+              header-border-variant="danger"
+            ></b-card-header>
+            <b-card-body
+              ><ol>
+                <li>
+                  <b>The Golden Rule</b> Treat your neighbor how you would treat
+                  yourself.
+                </li>
+                <li>
+                  So <b>NO</b> griefing, stealing, or uncalled-for player
+                  killing!
+                </li>
+                <li>Cut down trees entirely. No dangling logs in the air.</li>
+                <li>
+                  Don't make your animal farms contain more than 50 entities. In
+                  excess, this will lag the server. This is a Java Minecraft
+                  Bottleneck I do not know how to fix, so we have to deal with
+                  it.
+                </li>
+              </ol></b-card-body
+            >
+          </b-card>
         </b-col>
       </b-row>
+      <br />
       <b-row>
-        <b-col md="6" lg="4">
-          <b-card no-body="true">
-            <b-card-header header="How to Connect"></b-card-header>
-            <b-card-body>Server Field: <code>daroach.net</code></b-card-body>
+        <b-col md="6" lg="6">
+          <b-card no-body="true" border-variant="info">
+            <b-card-header
+              header="How to Connect"
+              class="display-4"
+              header-text-variant="info"
+              header-border-variant="info"
+            ></b-card-header>
+            <b-card-body>Server Address: <code>daroach.net</code></b-card-body>
+            <b-card-img
+              src="~/assets/mc_server_info.png"
+              fluid="true"
+            ></b-card-img>
+          </b-card>
+        </b-col>
+        <b-col md="6" lg="6">
+          <b-card no-body="true" border-variant="success">
+            <b-card-header
+              header="Server Features"
+              class="display-4"
+              header-text-variant="success"
+              header-border-variant="success"
+            ></b-card-header>
+            <b-card-body>
+              <ul>
+                <li>Vanilla Minecraft</li>
+                <li>
+                  I keep the server up to date with the latest official version.
+                </li>
+                <li>Large Biomes</li>
+                <li>Automatic daily server backups.</li>
+                <li>
+                  An instantiation of the offical minecraft docker container. -
+                  So really good performance!
+                </li>
+              </ul>
+            </b-card-body>
           </b-card>
         </b-col>
       </b-row>
