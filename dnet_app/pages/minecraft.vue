df3db5f61e3bfa33930123247b29000f9c8add25
gagany <gagandaroach@gmail.com>
2019-06-08 13:03:44 -0500

    finalize minecraft page
    add carousel, assets, new component, and background image

--- dnet_app/pages/minecraft.vue
+++ dnet_app/pages/minecraft.vue
@@ -5,117 +5,124 @@
     <PageTitle
       title="Daroach.net Minecraft"
       description="A fun place to procrastinate your life's responsibiliteis."
+      background="#56962c"
     />
 
-    <hr />
+    <div class="body">
+      <MinecraftCarousel />
 
-    <b-container>
-      <b-row id="rules">
-        <b-col>
-          <b-card
-            no-body="true"
-            border-variant="danger"
-            body-border-variant="danger"
-          >
-            <b-card-header
-              header="Server Rules"
-              class="display-4"
-              header-text-variant="danger"
-              header-border-variant="danger"
-            ></b-card-header>
-            <b-card-body
-              ><ul>
-                <li>
-                  <b>The Golden Rule</b> Treat your neighbor how you would treat
-                  yourself.
-                </li>
-                <li>
-                  So <b>NO</b> griefing, stealing, or uncalled-for player
-                  killing!
-                </li>
-                <li>Cut down trees entirely. No dangling logs in the air.</li>
-                <li>
-                  No animal farms with more than 50 animals.
-                </li>
-              </ul></b-card-body
+      <b-container>
+        <b-row id="rules">
+          <b-col>
+            <b-card
+              no-body="true"
+              border-variant="danger"
+              body-border-variant="danger"
             >
-          </b-card>
-        </b-col>
-      </b-row>
+              <b-card-header
+                header="Server Rules"
+                class="display-4"
+                header-text-variant="danger"
+                header-border-variant="danger"
+              ></b-card-header>
+              <b-card-body
+                ><ul>
+                  <li>
+                    <b>The Golden Rule</b> Treat your neighbor how you would
+                    treat yourself.
+                  </li>
+                  <li>
+                    So <b>NO</b> griefing, stealing, or uncalled-for player
+                    killing!
+                  </li>
+                  <li>Cut down trees entirely. No dangling logs in the air.</li>
+                  <li>
+                    No animal farms with more than 50 animals.
+                  </li>
+                </ul></b-card-body
+              >
+            </b-card>
+          </b-col>
+        </b-row>
+        <br />
+        <b-row>
+          <b-col md="6" lg="6">
+            <b-card no-body="true" border-variant="info">
+              <b-card-header
+                header="How to Connect"
+                class="display-4"
+                header-text-variant="info"
+                header-border-variant="info"
+              ></b-card-header>
+              <b-img fluid src="~/assets/mc/server_info.png"></b-img>
+              <b-card-body
+                >Server Address: <code>daroach.net</code></b-card-body
+              >
+            </b-card>
+          </b-col>
+          <b-col md="6" lg="6">
+            <b-card no-body="true" border-variant="success">
+              <b-card-header
+                header="Server Features"
+                class="display-4"
+                header-text-variant="success"
+                header-border-variant="success"
+              ></b-card-header>
+              <b-card-body>
+                <ul>
+                  <li>Vanilla Minecraft</li>
+                  <li>
+                    Always the latest Offical Version
+                  </li>
+                  <li>Large Biomes</li>
+                  <li>Fun & Cool Players</li>
+                </ul>
+              </b-card-body>
+            </b-card>
+            <br />
+            <b-card no-body="true" border-variant="warning">
+              <b-card-header
+                header="Server Technology"
+                class="display-4"
+                header-text-variant="warning"
+                header-border-variant="warning"
+              ></b-card-header>
+              <b-card-body>
+                <ul>
+                  <li>Automatic server backups every six hours.</li>
+                  <li>
+                    An instantiation of the
+                    <a href="https://hub.docker.com/r/itzg/minecraft-server/"
+                      ><code>itzg/minecraft-server</code></a
+                    >
+                    docker container.
+                  </li>
+                  <li>
+                    32GB of dedicated RAM space.
+                  </li>
+                  <li>
+                    Dual Intel Xeon L5520 @ 2.27GHz
+                  </li>
+                </ul>
+              </b-card-body>
+            </b-card>
+          </b-col>
+        </b-row>
+      </b-container>
       <br />
-      <b-row>
-        <b-col md="6" lg="6">
-          <b-card no-body="true" border-variant="info">
-            <b-card-header
-              header="How to Connect"
-              class="display-4"
-              header-text-variant="info"
-              header-border-variant="info"
-            ></b-card-header>
-            <b-img fluid src="~/assets/mc_server_info.png"></b-img>
-            <b-card-body>Server Address: <code>daroach.net</code></b-card-body>
-          </b-card>
-        </b-col>
-        <b-col md="6" lg="6">
-          <b-card no-body="true" border-variant="success">
-            <b-card-header
-              header="Server Features"
-              class="display-4"
-              header-text-variant="success"
-              header-border-variant="success"
-            ></b-card-header>
-            <b-card-body>
-              <ul>
-                <li>Vanilla Minecraft</li>
-                <li>
-                  Always the latest Offical Version
-                </li>
-                <li>Large Biomes</li>
-                <li>Fun & Cool Players</li>
-              </ul>
-            </b-card-body>
-          </b-card>
-          <br />
-          <b-card no-body="true" border-variant="warning">
-            <b-card-header
-              header="Server Technology"
-              class="display-4"
-              header-text-variant="warning"
-              header-border-variant="warning"
-            ></b-card-header>
-            <b-card-body>
-              <ul>
-                <li>Automatic server backups every six hours.</li>
-                <li>
-                  An instantiation of the
-                  <a href="https://hub.docker.com/r/itzg/minecraft-server/"
-                    ><code>itzg/minecraft-server</code></a
-                  >
-                  docker container.
-                </li>
-                <li>
-                  32GB of dedicated RAM space.
-                </li>
-                <li>
-                  Dual Intel Xeon L5520 @ 2.27GHz
-                </li>
-              </ul>
-            </b-card-body>
-          </b-card>
-        </b-col>
-      </b-row>
-    </b-container>
-    <br />
+    </div>
   </section>
 </template>
 
 <script>
 import Navbar from '~/components/Navbar.vue'
 import PageTitle from '~/components/PageTitle.vue'
+import MinecraftCarousel from '~/components/mc/MinecraftCarousel.vue'
 export default {
   components: {
     Navbar,
-    PageTitle
+    PageTitle,
+    MinecraftCarousel
   }
 }
 </script>
@@ -123,5 +130,13 @@ export default {
 <style scoped>
 #rules {
   text-align: center;
+}
+.body {
+  background-image: url('~assets/mc/dirt_background.png');
+  background-repeat: repeat;
+}
+#PageTitle {
+  background-image: url('~assets/mc/dirt_background.png');
+  background-repeat: repeat;
 }
 </style>
