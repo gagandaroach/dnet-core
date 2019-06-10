0a11ffa013f52ec983a2fc0fe8de714ca60444b1
gagany <gagandaroach@gmail.com>
2019-06-10 00:18:56 -0500

    about updates
    add inline tabs with components for site history, about me, and instagram embed.

---
+++ dnet_app/components/about/AboutTabs.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <b-tabs nav-class="bg-secondary" content-class="mt-3" align="center">
+      <b-tab title="The Homelab" title-link-class="btn-outline-light" active
+        ><HomeLab
+      /></b-tab>
+      <b-tab title="The Hoster" title-link-class="btn-outline-light"
+        ><AboutMe
+      /></b-tab>
+      <b-tab title="Site History" title-link-class="btn-outline-light"
+        ><SiteHistory
+      /></b-tab>
+    </b-tabs>
+  </div>
+</template>
+
+<script>
+import AboutMe from '~/components/about/AboutMe.vue'
+import HomeLab from '~/components/about/HomeLab.vue'
+import SiteHistory from '~/components/about/SiteHistory.vue'
+
+export default {
+  components: {
+    AboutMe,
+    HomeLab,
+    SiteHistory
+  }
+}
+</script>
+
+<style scoped></style>
