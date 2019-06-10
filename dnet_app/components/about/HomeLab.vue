79fb4928dbf5a4cf89fd906b8d8258d7439faf35
gagany <gagandaroach@gmail.com>
2019-06-10 00:18:56 -0500

    about updates
    add inline tabs with components for site history, about me, and instagram embed.

---
+++ dnet_app/components/about/HomeLab.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <InstagramEmbed />
+  </div>
+</template>
+
+<script>
+import InstagramEmbed from '~/components/about/InstagramEmbed.vue'
+
+export default {
+  components: {
+    InstagramEmbed
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ dnet_app/components/about/SiteHistory.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <b-container>
+      <h1>On the Todo List.</h1>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
+
+<style lang="scss" scoped></style>
