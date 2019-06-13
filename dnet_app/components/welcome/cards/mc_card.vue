e7bd7ca9358ee503ed6947a29bb2bd58dd367508
Gagan Daroach <gagandaroach@gmail.com>
2019-06-12 23:03:37 -0500

    homelab card, minecraft card, and aboutme card
    images, card deck margins

--- dnet_app/components/welcome/cards/mc_card.vue
+++ dnet_app/components/welcome/cards/mc_card.vue
@@ -1,25 +1,10 @@
 <template>
   <section>
-    <!-- <b-card
-      no-body="true"
-      border-variant="dark"
-      bg-variant="dark"
-      text-variant="light"
-    >
-      <b-card-header
-        header="Minecraft Server"
-        class="h1"
-        header-text-variant="primary"
-        header-border-variant="primary"
-      ></b-card-header>
-      <b-card-img src="~/assets/mc/gagany.png" />
-    </b-card> -->
     <b-card
       title="Minecraft Server"
       img-src="~/assets/mc/gagany.png"
       img-alt="Minecraft Server Screenshot"
       img-top
-      style="max-width: 40%;"
       class="mb-2"
     >
       <b-card-text>
