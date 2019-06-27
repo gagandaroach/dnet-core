0e009b446691d348000b6a5ee5272ec7968d5d1f
gagany <gagandaroach@gmail.com>
2019-06-27 02:13:11 -0500

    navbar toggleable, skinned with font, and split up n centered :ok_emoji:

--- dnet_app/components/Navbar.vue
+++ dnet_app/components/Navbar.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
-    <b-navbar class="main" type="dark" variant="primary" sticky>
+    <b-navbar
+      class="main"
+      type="dark"
+      variant="primary"
+      toggleable="lg"
+      sticky
+      print
+    >
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="">
           <b-nav-item
@@ -23,8 +30,9 @@
           >
         </b-navbar-nav>
       </b-collapse>
-      <b-navbar-brand to="/">daroach.net</b-navbar-brand>
-      <b-collapse id="nav-collapse2" is-nav>
+
+      <b-navbar-brand class="brand" to="/">daroach.net</b-navbar-brand>
+      <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto" right>
           <b-nav-item
             v-b-popover.hover.bottom="'A Personal Dropbox Server'"
@@ -47,37 +55,10 @@
           >
         </b-navbar-nav>
       </b-collapse>
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-      <b-navbar-toggle target="nav-collapse2"></b-navbar-toggle>
-      <!-- <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="ml-auto">
-            <b-nav-item-dropdown id="nav-admin-dropdown" text="Services" right>
-              <b-dropdown-item
-                v-b-popover.hover.left="'A Personal Dropbox Server'"
-                href="https://cloud.daroach.net"
-                >Cloud
-              </b-dropdown-item>
-              <b-dropdown-item
-                v-b-popover.hover.left="'A Personal Netflix Server'"
-                href="https://media.daroach.net"
-                >Plex
-              </b-dropdown-item>
-              <b-dropdown-item
-                v-b-popover.hover.left="'Minecraft server for the boyz.'"
-                to="/minecraft"
-                >Minecraft
-              </b-dropdown-item>
-              <b-dropdown-divider></b-dropdown-divider>
-              <b-dropdown-item
-                v-b-popover.hover.left="
-                  'Allows for administration of the homelab\'s docker containers.'
-                "
-                href="https://portainer.daroach.net"
-                >Portainer
-              </b-dropdown-item>
-            </b-nav-item-dropdown>
-          </b-navbar-nav>
-        </b-collapse> -->
+      <b-navbar-brand v-show="showBrand" class="brand" to="/"
+        >daroach.net</b-navbar-brand
+      >
+      <b-navbar-toggle v-model="toggle" target="nav-collapse"></b-navbar-toggle>
     </b-navbar>
   </div>
 </template>
@@ -90,8 +71,13 @@ export default {
 
 <style scoped>
 .main {
+  font-family: 'Bungee', sans-serif;
   /* margin-top: 40px; */
   text-align: center;
   /* padding-block: 30px; */
+}
+.brand {
+  text-align: center;
+  font-family: 'Bungee', sans-serif;
 }
 </style>
