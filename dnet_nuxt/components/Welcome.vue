f0a1a13e1f0f99cd2c11348979b97314ac1c7eec
gagany <gagandaroach@gmail.com>
2019-08-07 00:35:25 -0500

    title updates
    use calculated font size to grow proportionally with screen view width.
    place into v-container and v-layout objects

--- dnet_nuxt/components/Welcome.vue
+++ dnet_nuxt/components/Welcome.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    <h1 :class=titleClass >DAROACH.NET</h1>
-    <h2 >Hello Visitor {{ visitorNumber }}</h2>
-    <hr />
-    s
-  </div>
+  <v-container fluid fill-height height="100vh">
+    <v-layout align-center justify-center>
+      <div class="body">
+        <h1 :class="titleClass">DAROACH.NET</h1>
+        <hr />
+        <h2>Welcome Visitor {{ visitorNumber }}</h2>
+      </div>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -16,18 +19,30 @@ export default {
   },
   method: { },
   computed: {
-    titleClass () {
-      switch (this.$vuetify.breakpoint.name) {
-        case 'xs': return 'display-2'
-        case 'sm': return 'display-3'
-        case 'md': return 'display-4'
-        case 'lg': return 'display-4'
-        case 'xl': return 'display-4'
-        default: return 'display-2'
-      }
-    }
+    // titleClass () {
+    //   // switch (this.$vuetify.breakpoint.name) {
+    //   //   case 'xs': return 'display-2'
+    //   //   case 'sm': return 'display-3'
+    //   //   case 'md': return 'display-4'
+    //   //   case 'lg': return 'display-4'
+    //   //   case 'xl': return 'display-4'
+    //   //   default: return 'display-4'
+    //   // }
+    // }
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.body {
+  // Minimum 16x, and grows more slowly
+  // (half the rate of screen growth)
+  font-size: calc(16px + 0.5vw);
+}
+h1 {
+  font-size: calc(1.5em + 5.0vw);
+}
+h2 {
+  font-size: calc(1.2em + 1.0vw);
+}
+</style>
