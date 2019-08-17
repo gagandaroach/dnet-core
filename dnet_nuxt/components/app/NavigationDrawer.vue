80c8f570a21be5968e4c7ef7b356978f94c18009
gagany <gagandaroach@gmail.com>
2019-08-17 14:25:06 -0500

    move nav drawer list to component

---
+++ dnet_nuxt/components/app/NavigationDrawer.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+      <v-list-item-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.title" />
+      </v-list-item-content>
+    </v-list-item>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Blog',
+          to: '/inspire'
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'About',
+          to: '/inspire'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- dnet_nuxt/layouts/default.vue
+++ dnet_nuxt/layouts/default.vue
@@ -2,14 +2,7 @@
   <v-app dark>
     <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" clipped fixed app>
       <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
+        <NavDrawer />
       </v-list>
       <template v-slot:append>
         <div class="pa-2">
@@ -24,8 +17,12 @@
       <v-toolbar-title v-text="title" />
       <v-spacer />
       <v-toolbar-items>
-        <v-btn text to="/">Home</v-btn>
-        <v-btn text to="/about">About Me</v-btn>
+        <v-btn text to="/">
+          Home
+        </v-btn>
+        <v-btn text to="/about">
+          About Me
+        </v-btn>
       </v-toolbar-items>
     </v-app-bar>
     <v-content>
@@ -35,36 +32,24 @@
     </v-content>
     <v-footer :fixed="fixed" app>
       <span><a href="https://github.com/gagandaroach/daroachnet">Website Source Code</a></span>
-      <v-spacer></v-spacer>
+      <v-spacer />
       <span>Gagan Daroach &copy; 2019</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
+import NavDrawer from '~/components/app/NavigationDrawer.vue'
+
 export default {
+  components: {
+    NavDrawer
+  },
   data () {
     return {
       drawer: false,
       fixed: false,
       miniVariant: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Blog',
-          to: '/inspire'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'About',
-          to: '/inspire'
-        }
-      ],
       title: 'daroach.net'
     }
   }
