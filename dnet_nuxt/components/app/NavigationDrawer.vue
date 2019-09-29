63a69e68becdce255f5d4d7860b2bcb966765104
gagany <gagandaroach@gmail.com>
2019-09-29 13:39:57 -0500

    update drawer with links to services

--- dnet_nuxt/components/app/NavigationDrawer.vue
+++ dnet_nuxt/components/app/NavigationDrawer.vue
@@ -2,7 +2,17 @@
 
 <template>
   <div>
+    <v-subheader>WEB PAGES</v-subheader>
     <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
+      <v-list-item-action>
+        <v-icon>{{ item.icon }}</v-icon>
+      </v-list-item-action>
+      <v-list-item-content>
+        <v-list-item-title v-text="item.title" />
+      </v-list-item-content>
+    </v-list-item>
+    <v-subheader>WEB SERVICES</v-subheader>
+    <v-list-item v-for="(item, i) in services" :key="i" :to="item.to" router exact>
       <v-list-item-action>
         <v-icon>{{ item.icon }}</v-icon>
       </v-list-item-action>
@@ -32,16 +42,23 @@ export default {
           icon: 'mdi-information',
           title: 'About',
           to: '/about'
+        }
+      ],
+      services: [
+        {
+          icon: 'mdi-cloud-sync',
+          title: 'NextCloud',
+          to: '/services/cloud'
+        },
+        {
+          icon: 'mdi-api',
+          title: 'DTECH API',
+          to: '/services/cloud'
         },
         {
           icon: 'mdi-gamepad-variant',
           title: 'Minecraft Server',
           to: '/minecraft'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Vuetify Inspire',
-          to: '/inspire'
         }
       ]
     }
