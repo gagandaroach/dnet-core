71cfa56b8b94538aa15a08603f7d080e0b8ef0b3
gagany <gagandaroach@gmail.com>
2019-10-19 01:54:57 -0500

    update page title. remove title template.
    nuxt update page title.

--- dnet_nuxt/pages/welcome/index.vue
+++ dnet_nuxt/pages/welcome/index.vue
@@ -43,9 +43,15 @@ export default {
   },
   data () {
     return {
+      title: 'Welcome',
       PageTitle: 'Welcome to Daroach.net',
       Description: 'Thanks for connecting to my homelab!',
       Alert: 'Hello. My name is Gagan Daroach, and you have used the power of the internet to connect to a box in my apartment. Thanks for coming through! I am growing this website to contain more fun and informative things every day. Do feel free to explore around, and poke on every button.'
+    }
+  },
+  head () {
+    return {
+      title: this.title
     }
   }
 }
