845152b8bc8c6b99ab776f69bcf246899e922640
Gagan Daroach <gagandaroach@gmail.com>
2019-11-03 22:15:02 -0600

    update footer spacing

--- dnet_nuxt/components/Footer.vue
+++ dnet_nuxt/components/Footer.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <footer class="app-footer">
-      <span><a href="https://github.com/gagandaroach/daroachnet">Website Source Code</a></span>
-      <span>Gagan Daroach &copy; 2019</span>
+      <b-row>
+        <b-col class="left ml-1">
+          <span><a href="https://github.com/gagandaroach/daroachnet">Website Source Code</a></span>
+        </b-col>
+        <b-col class="right mr-1">
+          <span>Gagan Daroach &copy; 2019</span>
+        </b-col>
+      </b-row>
     </footer>
   </div>
 </template>
@@ -14,5 +20,15 @@ export default {
 </script>
 
 <style>
-
+.app-footer, a, a:hover {
+  background-color: var(--primary);
+  color: white;
+  height: 32px;
+}
+.left {
+  text-align: left;
+}
+.right {
+  text-align: right;
+}
 </style>
