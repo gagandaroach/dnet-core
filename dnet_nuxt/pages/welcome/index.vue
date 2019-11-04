9eb33bbf87c56cdcce87e3fe4a06915771ec9fd0
gagany <gagandaroach@gmail.com>
2019-11-03 21:12:28 -0600

    add pagetitle component

--- dnet_nuxt/pages/welcome/index.vue
+++ dnet_nuxt/pages/welcome/index.vue
@@ -1,20 +1,38 @@
 <template>
-  <div class="container">
+  <div>
     <div>
-      <h1 class="title">
-        Welcome
-      </h1>
-      <h2 class="subtitle">
-        welcome page
-      </h2>
+      <PageTitle
+        :title="PageTitle"
+        :description="Description"
+        :padding-bottom="20"
+        background-color="#00000000"
+        text-color="#FFF"
+      />
     </div>
   </div>
 </template>
 
 <script>
+import PageTitle from '~/components/PageTitle.vue'
+
 export default {
   layout: 'default',
-  components: { }
+  components: {
+    PageTitle
+  },
+  data () {
+    return {
+      title: 'Welcome',
+      PageTitle: 'Welcome to Daroach.net',
+      Description: 'Thanks for connecting to my homelab!',
+      Alert: 'Hello. My name is Gagan Daroach, and you have used the power of the internet to connect to a box in my apartment. Thanks for coming through! I am growing this website to contain more fun and informative things every day. Do feel free to explore around, and poke on every button.'
+    }
+  },
+  head () {
+    return {
+      title: this.title
+    }
+  }
 }
 </script>
 
