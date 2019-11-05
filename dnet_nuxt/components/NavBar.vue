2a247ad54817cdc08454c2cd1afd440e9bac617a
gagany <gagandaroach@gmail.com>
2019-11-04 21:58:52 -0600

    vue vfor example
    update navbar to use data list for menu links. v-for through bootstrap menu items

--- dnet_nuxt/components/NavBar.vue
+++ dnet_nuxt/components/NavBar.vue
@@ -2,18 +2,15 @@
   <div>
     <b-navbar toggleable="md" type="dark" variant="primary" class="main" sticky>
       <b-navbar-brand to="/">
-        daroach.net
+        {{ brand }}
       </b-navbar-brand>
 
       <b-navbar-toggle target="nav-collapse" />
 
       <b-collapse id="nav-collapse" is-nav>
         <b-navbar-nav class="ml-auto">
-          <b-nav-item to="/welcome" active-class="active">
-            Welcome
-          </b-nav-item>
-          <b-nav-item to="/about/me" active-class="active">
-            About
+          <b-nav-item v-for="(item, index) in nav_items" :to="item.link" :key="index" active-class="active">
+            {{ item.text }}
           </b-nav-item>
         </b-navbar-nav>
       </b-collapse>
@@ -23,7 +20,15 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      brand: 'daroach.net',
+      nav_items: [
+        { text: 'Welcome', link: '/welcome' },
+        { text: 'About', link: '/about/me' }
+      ]
+    }
+  }
 }
 </script>
 
