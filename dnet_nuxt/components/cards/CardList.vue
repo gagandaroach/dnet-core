99a8a89e284a4f4830e68cde8e77ead6f5773c04
Gagan Daroach <gagandaroach@gmail.com>
2019-11-06 00:35:05 -0600

    create card components

---
+++ dnet_nuxt/components/cards/CardList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-card
+      no-body
+    >
+      <b-card-body>
+        <b-card-title>Card Title</b-card-title>
+        <b-card-sub-title class="mb-2">
+          Card Sub Title
+        </b-card-sub-title>
+        <b-card-text>
+          Some quick example text to build on the card title and make up the bulk of the card's
+          content.
+        </b-card-text>
+      </b-card-body>
+      <b-list-group v-for="(item, index) in items" :key="index" flush>
+        <b-list-group-item>{{ item }}</b-list-group-item>
+      </b-list-group>
+    </b-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        'List item one',
+        'List item two',
+        'List item three'
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
