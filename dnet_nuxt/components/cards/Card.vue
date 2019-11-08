b277e02f76af497579e8de19107e05d700c0ad27
Gagan Daroach <gagandaroach@gmail.com>
2019-11-07 20:38:33 -0600

    add card and cardlist components

--- dnet_nuxt/components/cards/Card.vue
+++ dnet_nuxt/components/cards/Card.vue
@@ -1,19 +1,17 @@
 <template>
   <div>
     <b-card
-      title="Card Title"
-      img-src="https://picsum.photos/600/300/?image=25"
-      img-alt="Image"
+      :title="title"
+      :img-src="img_src"
+      :img-alt="img_alt"
       img-top
-      tag="article"
       class="mb-2"
     >
       <b-card-text>
-        Some quick example text to build on the card title and make up the bulk of the card's content.
+        {{ text }}
       </b-card-text>
-
-      <b-button href="#" variant="primary">
-        Go somewhere
+      <b-button :to="btnHref" :variant="btnVariant">
+        {{ btnText }}
       </b-button>
     </b-card>
   </div>
@@ -21,7 +19,49 @@
 
 <script>
 export default {
-
+  props: {
+    col: {
+      type: String,
+      default: 'col-12'
+    },
+    title: {
+      type: String,
+      default: 'card title'
+    },
+    subtitle: {
+      type: String,
+      default: 'card subtitle'
+    },
+    text: {
+      type: String,
+      default: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.'
+    },
+    imgSrc: {
+      type: String,
+      default: 'https://picsum.photos/600/300/?image=25'
+    },
+    imgAlt: {
+      type: String,
+      default: 'image_alt_text'
+    },
+    btnVariant: {
+      type: String,
+      default: 'primary'
+    },
+    btnHref: {
+      type: String,
+      default: '#'
+    },
+    btnText: {
+      type: String,
+      default: 'Go somewhere'
+    }
+  },
+  data () {
+    return {
+      nothing: 0
+    }
+  }
 }
 </script>
 
