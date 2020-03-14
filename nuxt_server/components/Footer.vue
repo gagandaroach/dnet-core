fc2b89015f97d932a522a59df71a9139c9f02699
gagany <gagandaroach@gmail.com>
2020-03-14 10:45:22 -0500

    vuetify footer
    use props for data. add email out link on source code

--- nuxt_server/components/Footer.vue
+++ nuxt_server/components/Footer.vue
@@ -3,10 +3,10 @@
     <footer class="app-footer">
       <b-row>
         <b-col class="left ml-1">
-          <span><a v-b-tooltip.hover title="Complete full stack code examples for this website are available on Github. See the complete history of this website, one changed line at a time." href="https://github.com/gagandaroach/daroachnet">Website Source Code</a></span>
+          <span><a v-b-tooltip.hover :title="source_code_popup_text" href="https://github.com/gagandaroach/daroachnet">{{ source_code_link_title }}</a></span>
         </b-col>
         <b-col class="right mr-1">
-          <span>Gagan Daroach &copy; 2019</span>
+          <a v-b-tooltip.hover :title="copyright_popup_text" href="mailto:gagandaroach@gmail.com?Subject=Hello Gagan, I like daroach.net!" target="_top"><span>Gagan Daroach &copy; 2019</span></a>
         </b-col>
       </b-row>
     </footer>
@@ -15,7 +15,13 @@
 
 <script>
 export default {
-
+  data () {
+    return {
+      source_code_popup_text: 'Complete full stack code example for this website are available on Github. Also see the complete development history of this website, one changed line at a time.',
+      source_code_link_title: 'Website Source Code',
+      copyright_popup_text: 'Questions, Comments, or Concerns? Email me :).'
+    }
+  }
 }
 </script>
 
