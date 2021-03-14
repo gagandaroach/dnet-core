49af22a2ac57fde51962221225f00144b01dd08e
gagany <gagandaroach@gmail.com>
2021-03-14 12:03:26 +0000

    split up services into domain based folders, split up docker-compose file

--- cloud.daroach.net/Nextcloud.Dockerfile
+++ cloud.daroach.net/Nextcloud.Dockerfile
@@ -1,4 +1,4 @@
-FROM nextcloud:apache
+FROM nextcloud:21-apache
 
 RUN set -ex; \
     \
--- media_server/redeploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker-compose build --pull
-docker-compose down
-docker-compose up -d
-docker-compose logs -f
