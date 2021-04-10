9ef629219480af3eaae37f13a1e54ed0426ba6ad
gagany <gagandaroach@gmail.com>
2021-04-10 02:15:23 -0500

    add curl api tests bash script

--- daroach.net/dnet2_api/test.sh
+++ daroach.net/dnet2_api/test.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-curl -X GET 
+ADDRESS="localhost:3001"
+API_KEY="apikey"
+CURL_OPTS="-s"
+
+echo GET $ADDRESS
+curl $CURL_OPTS -X GET $ADDRESS
+echo 
+echo
+
+# echo GET /dnet no key
+# curl $CURL_OPTS -X GET $ADDRESS/dnet
+# echo 
+# echo
+
+echo GET /dnet
+curl $CURL_OPTS -X GET $ADDRESS/dnet?api-key=$API_KEY
+echo 
+echo
+
+echo GET /dnet/hits
+curl $CURL_OPTS -X GET $ADDRESS/dnet/hits?api-key=$API_KEY
+echo 
+echo
+
