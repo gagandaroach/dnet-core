4cac44db490bfcc5fdb07f7beb063acdb9da3c72
gagany <gagandaroach@gmail.com>
2021-04-11 08:48:33 -0500

    add vue-chartjs

--- daroach.net/dnet2/components/WebStatistics.vue
+++ daroach.net/dnet2/components/WebStatistics.vue
@@ -1,8 +1,10 @@
 <template>
   <div class="container dnet-card">
-    <h1 class="text-lg">Web Statistics</h1>
-    <div>
-      {{ this.hits }}
+    <h1 class="flex text-lg">Web Statistics</h1>
+    <div class="flex">
+      <client-only>
+        <BarChart :data="data" :options="chartOptions" />
+      </client-only>
     </div>
   </div>
 </template>
@@ -13,6 +15,39 @@ export default Vue.extend({
   data() {
     return {
       hits: [],
+      chartOptions: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [
+          {
+            label: "# of Votes",
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
     };
   },
   mounted() {
