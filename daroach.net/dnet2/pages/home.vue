a007cbd54807492bc6188ff4706b6b2f5298931f
gagany <gagandaroach@gmail.com>
2021-04-12 10:15:15 -0500

    size charts + cards on home

--- daroach.net/dnet2/pages/home.vue
+++ daroach.net/dnet2/pages/home.vue
@@ -3,7 +3,10 @@
     <h1 class="home-header-1">Welcome to Daroach.net</h1>
     <AboutGagan />
     <h1 class="home-header-1">WebServer Traffic</h1>
-    <WebStatistics />
+    <div class="flex flex-auto">
+      <total-page-hits />
+      <timely-page-hits v-if="false"/>
+    </div>
     <h1 class="home-header-1">Open Source Projects</h1>
     <nuxt-content :document="page" class="dnet-content"></nuxt-content>
     <Content />
@@ -11,7 +14,11 @@
 </template>
 
 <script>
+import TimelyPageHits from "../components/Charts/TimelyPageHits.vue";
+import TotalPageHits from "../components/Charts/TotalPageHits.vue";
+
 export default {
+  components: { TotalPageHits, TimelyPageHits },
   layout: "default",
   async asyncData({ $content }) {
     const page = await $content("home").fetch();
