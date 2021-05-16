3a6334034113aee168774d9232f85e28304e9e4f
gagan <gagandaroach@gmail.com>
2021-05-15 22:44:07 -0500

    update packges + set max width on about gagan

--- daroach.net/nuxt_webserver/components/AboutGagan.vue
+++ daroach.net/nuxt_webserver/components/AboutGagan.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="container dnet-card">
+  <div class="flex max-w-3xl dnet-card">
     <div class="flex flex-col space-y-2">
       <h1>Gagandeep Bishamber Daroach</h1>
       <h2>Daroach.net Maintainer and Host</h2>
+      <div class="flex flex-shrink-0 p-5">
+        <img
+          class="object-scale-down h-auto w-auto"
+          src="/outside_tree_home.jpg"
+        />
+      </div>
       <hr class="my-2" />
       <p>
         I am an aspiring Software Engineer, hoping to
         <a href="https://youtu.be/J-GVd_HLlps?t=32">
           make the world a better place.
         </a>
-        With this website, I hope to create a place for people to share stories,
-        knowledge, information, and data.
+        I hope to grow daroach.net into a place for people to share stories,
+        knowledge, information, and data. Here you can find information about myself and links to my open source projects.
       </p>
-      <div class="flex flex-shrink-0">
-        <img
-          class="object-scale-down h-auto w-auto"
-          src="/outside_tree_home.jpg"
-        />
-      </div>
       <div class="">
-        <!-- <p class="">My Social Media</p> -->
+        <p class="">My Social Media</p>
         <div class="my-2 justfiy-around">
           <a
             v-for="social in this.socials"
