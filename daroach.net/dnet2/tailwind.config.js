00853d5427e737bff73370634d8fc57ced4952aa
gagany <gagandaroach@gmail.com>
2021-05-15 19:20:14 -0500

    add background gradiant animation

--- daroach.net/dnet2/tailwind.config.js
+++ daroach.net/dnet2/tailwind.config.js
@@ -15,7 +15,45 @@ module.exports = {
       'serif': ['Roboto Slab', 'ui-serif', 'Georgia'],
       'mono': ['Ubuntu Mono', 'ui-monospace', 'SFMono-Regular'],
     },
-    extend: {},
+    extend: {
+      'animation': {
+        'gradient-x': 'gradient-x 15s ease infinite',
+        'gradient-y': 'gradient-y 15s ease infinite',
+        'gradient-xy': 'gradient-xy 15s ease infinite',
+      },
+      'keyframes': {
+        'gradient-y': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'center top'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'center center'
+          }
+        },
+        'gradient-x': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        },
+        'gradient-xy': {
+          '0%, 100%': {
+            'background-size': '400% 400%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          }
+        }
+      }
+    },
   },
   variants: {
     extend: {},
