a211757c35b83a6d90e53f551edc75ff80cb97dd
gagany <gagandaroach@gmail.com>
2021-05-16 03:31:04 +0000

    change cardgrid to 2 col md, rename dnet2 to nuxt_webserverg

--- daroach.net/nuxt_webserver/components/CardGrid.vue
+++ daroach.net/nuxt_webserver/components/CardGrid.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="container mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-10"
+    class="container mx-auto grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-2 gap-10"
   >
     <div
       v-for="item of content"
