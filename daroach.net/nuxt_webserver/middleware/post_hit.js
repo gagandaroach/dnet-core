94039011bdb61e10a77fdb76d330e834dc164c2a
gagany <gagandaroach@gmail.com>
2022-08-29 06:37:01 +0000

    restructure - destructive change of repo hierarchy into subdomains

--- daroach.net/nuxt_webserver/middleware/post_hit.js
+++
@@ -1,7 +0,0 @@
-// param is context
-export default function ({ $axios, route, req }) {
-  return $axios.post('hits?api-key=apikey', {
-    host: req != undefined ? req.headers.host : window.location.host,
-    route: route.path
-  }, { })
-}
